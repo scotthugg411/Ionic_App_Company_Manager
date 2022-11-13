1559e067bbcb5dfca63680e0cd31448e0f7fee8d
Scott Huggins <scotthugg.411@gmail.com>
2022-11-13 14:43:07 -0500

    feat: adding companies example api call

--- client/src/pages/Companies.tsx
+++ client/src/pages/Companies.tsx
@@ -1,10 +1,51 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import { ListContainer } from '../components/ListContainer';
 import './styles/Companies.css';
 
+/**
+ * This is only an example on how to use hooks for api data fetching
+ * TODO make an abstract service to call api and use
+ * 
+ */
 const Companies: React.FC = () => {
+  const [companies,setCompanies]=useState([]);
+
+  useEffect(() => {
+    const apiURL = 'http://localhost:4000/api/companies';
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch(apiURL);
+            const json = await response.json();
+            console.log(json);
+            setCompanies(json);
+        } catch (error) {
+            console.log("error", error);
+        }
+    };
+
+    fetchData();
+}, []);
+
+const companiesList = companies.map(
+  (element:any) => {
+      return (
+          <ul >
+              <li>
+                Company ID:{element.companyId}
+              </li>
+              <li>
+                Name:{element.name}
+              </li>
+          </ul>
+      )
+  }
+)
+
+
   return (
     <IonPage>
       <IonHeader>
@@ -19,10 +60,12 @@ const Companies: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Companies page" message="Whatever" />
+
         <ListContainer/>
       </IonContent>
+      <IonContent>{companiesList}</IonContent>
     </IonPage>
   );
-};
+  };
 
 export default Companies;
